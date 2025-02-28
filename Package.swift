5765b79c3d95cfddc42411e88686ff5937496976
Aleksandra Mikusek <xamikusek@gmail.com>
2025-03-01 00:49:47 +0100

    test 2

--- Package.swift
+++ Package.swift
@@ -35,7 +35,7 @@ let package = Package(
             ],
             swiftSettings: [
         		.unsafeFlags(["-enable-library-evolution"])
-    		]
+    		],
             resources: [.process("Resources")]
         ),
         .testTarget(
