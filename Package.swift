03aaee1256d92134f1c1dc28553e027dc1823157
Aleksandra Mikusek <xamikusek@gmail.com>
2025-03-01 01:00:01 +0100

    test 5

--- Package.swift
+++ Package.swift
@@ -1,31 +1,45 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
     name: "analytics-swift",
     platforms: [
-        .iOS(.v11),
-        .macOS(.v10_12),
-        .tvOS(.v11),
-        .watchOS(.v6)
+        .macOS("10.15"),
+        .iOS("13.0"),
+        .tvOS("13.0"),
+        .watchOS("7.1"),
+        .visionOS("1.0")
     ],
     products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "Segment",
-            targets: ["Segment"]
-        ),
+            targets: ["Segment"]),
     ],
-    dependencies: [],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/segmentio/sovran-swift.git", from: "1.1.1"),
+        .package(url: "https://github.com/segmentio/jsonsafeencoding-swift.git", from: "2.0.0")
+    ],
     targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Segment",
-            dependencies: [],
+            dependencies: [
+                .product(name: "Sovran", package: "sovran-swift"),
+                .product(name: "JSONSafeEncoding", package: "jsonsafeencoding-swift")
+            ],
+            resources: [.process("Resources")].
             swiftSettings: [
                 .unsafeFlags(["-enable-library-evolution"])
             ]
         ),
         .testTarget(
-            name: "SegmentTests",
+            name: "Segment-Tests",
             dependencies: ["Segment"]
         ),
     ]
